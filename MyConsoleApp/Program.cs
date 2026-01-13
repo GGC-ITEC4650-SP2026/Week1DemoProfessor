d4a94d3cdbeed75b5db27e6b054ae27c961bff00
Sebastien Siva <ssiva@ggc.edu>
2026-01-13 21:45:19 +0000

    Small Ex

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello 4650!");
         Fruit f = new Fruit("Pear", 1.05);
         Console.WriteLine(f);
     }
