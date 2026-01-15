0fc285884303205953e3429b0686838a042c66cc
Sebastien Siva <ssiva@ggc.edu>
2026-01-15 20:55:43 +0000

    Test Commit

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -4,8 +4,30 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello 4650!");
-        Fruit f = new Fruit("Pear", 1.05);
-        Console.WriteLine(f);
+        //Console.WriteLine("Hello 4650!");
+        //Fruit f = new Fruit("Pear", 1.05);
+        //Console.WriteLine(f);
+
+        Random rgen = new Random();
+        int secret = rgen.Next(1, 11);
+        int guess = -1;
+        bool gameover = false;
+        while(!gameover)
+        {
+            // Get user input
+            Console.Write("Enter num 1-10: ");   
+            guess = int.Parse(Console.ReadLine());
+
+            //Update state
+            if(guess == secret)
+            {
+                gameover = true;
+            }
+            else {
+                //Render
+                Console.WriteLine("Not today!");
+            }
+        }
+        Console.WriteLine("Winner Winner Chicken Dinner!");
     }
 }
